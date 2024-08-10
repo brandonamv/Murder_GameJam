b6ab89023cd0b1ab0f265233df9ea2b19f0492c6
Ricardo <ricardosuna97@gmail.com>
2024-08-10 01:33:57 -0400

    collision

--- player.gd
+++ player.gd
@@ -7,11 +7,21 @@ var gravity = 9
 func _physics_process(delta):
 	direction = GLOBAL.get_axis()
 	
+	var newPosition = Vector3.ZERO
+	
 	velocity.x = direction.x * speed
 	velocity.z = direction.y * speed
 	
 	if !is_on_floor():
 		velocity.y -= gravity
+		
+	var player = get_node(".")
+	
+	if player.position.y < -200 :
+		player.position.x = 0
+		player.position.y = 2
+		player.position.z = 0
+		
 	move_and_slide()
 
 
